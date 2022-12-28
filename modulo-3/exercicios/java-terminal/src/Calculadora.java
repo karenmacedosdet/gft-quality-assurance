bd9348260313184d1f6965d7139a70a36668807b
kdevmacedo <kdevmacedo@gmail.com>
2022-12-28 17:11:05 -0300

    Atualiza MModulo 3

---
+++ modulo-3/exercicios/java-terminal/src/Calculadora.java
@@ -0,0 +1,23 @@
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matemáticas entre números inteiros.
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação destas classes para desfrutar dos recursos oferecidos pela autora.
+ *
+ * @author Karen Macedo
+ * @version 1.0
+ * @since 2022-12-26
+ */
+
+public class Calculadora {
+  /**
+   * Este método é utilizado para somar dois números inteiros.
+   * @param numeroUm é o primeiro parâmetro para o método somar.
+   * @param numeroDois é o segundo parâmetro para o método somar.
+   * @return int este retorno é a soma dos dois números inteiros.
+   */
+
+  public int somar (int numeroUm, int numeroDois) {
+    return numeroUm + numeroDois;
+  }
+}
---
+++ modulo-3/exercicios/java-terminal/src/Comentarios.java
@@ -0,0 +1,16 @@
+public class Comentarios {
+
+  public static void main(String[] args) {
+
+  }
+
+  public int somaMultiplica (int n, int x, String multiplicacao) {
+    int resultado = 0;
+    if (multiplicacao == "M") {
+      resultado = n * x;
+    } else {
+      resultado = n + x;
+    }
+    return resultado;
+  }
+}
