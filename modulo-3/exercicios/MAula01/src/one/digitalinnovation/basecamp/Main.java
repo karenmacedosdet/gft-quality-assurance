652d080ad7bfde156d1f3aa5fc27baf932bdd45d
kdevmacedo <kdevmacedo@gmail.com>
2023-01-05 16:06:26 -0300

    Atualiza Modulo 3

---
+++ modulo-3/exercicios/MAula01/src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,29 @@
+package one.digitalinnovation.basecamp;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Calculadora
+        System.out.println("Exercício da Calculadora:");
+        Calculadora.soma(3, 6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7, 8);
+        Calculadora.divisao(5, 2.5);
+
+        //Mensagem
+        System.out.println("Exercício da Mensagem:");
+        Mensagem.obtermensagem(9);
+        Mensagem.obtermensagem(14);
+        Mensagem.obtermensagem(1);
+
+        //Empréstimo
+        System.out.println("Exercício do Empréstimo:");
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000, 5);
+    }
+}
+
---
+++ modulo-3/exercicios/MAula01/src/one/digitalinnovation/basecamp/Mensagem.java
@@ -0,0 +1,5 @@
+package one.digitalinnovation.basecamp;
+
+public class Mensagem {
+
+}
