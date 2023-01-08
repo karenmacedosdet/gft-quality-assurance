ac687bd2eebf0a6901097678cfa116837d4e462b
kdevmacedo <kdevmacedo@gmail.com>
2023-01-08 20:24:38 -0300

    Atualiza Modulo 5

--- modulo-5/exercicios/OOAula03.1/src/one/digitalinnovation/oo/Carro.java
+++ modulo-5/exercicios/OOAula03.1/src/one/digitalinnovation/oo/Carro.java
@@ -1,7 +1,7 @@
 package one.digitalinnovation.oo;
 
 /**
- * Classe de exemplo para o exercício da Aula 2 de Orientação a Objetos.
+ * Classe de exemplo para o exercício da Aula 3 de Orientação a Objetos.
  */
 
 public class Carro {
