b5e7456760ff4f38ad66df5355ce93b2243e1b6c
kdevmacedo <kdevmacedo@gmail.com>
2023-01-09 18:58:51 -0300

    Atualiza Modulo 5

---
+++ modulo-5/exercicios/OOAula04.3/src/one/digitalinnovation/oo/ClasseFilha1.java
@@ -0,0 +1,14 @@
+package one.digitalinnovation.oo;
+
+/**
+ * Classe de exemplo para o exercício da Aula 4 de Orientação a Objetos.
+ */
+
+public class ClasseFilha1 extends ClasseMae {
+
+    @Override
+    void metodo1() {
+        System.out.println("Método 1 da Classe Filha 1");
+    }
+
+}
