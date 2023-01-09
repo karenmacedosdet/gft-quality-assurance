c83dff393158f69cd6fd3852c562ad815c5dc20f
kdevmacedo <kdevmacedo@gmail.com>
2023-01-09 18:41:41 -0300

    Atualiza Modulo 5

---
+++ modulo-5/exercicios/OOAula04.2/src/one/digitalinnovation/oo/Faxineiro.java
@@ -0,0 +1,10 @@
+package one.digitalinnovation.oo;
+
+/**
+ * Classe de exemplo para o exercício da Aula 4 de Orientação a Objetos.
+ */
+
+public class Faxineiro extends Funcionario {
+
+
+}
---
+++ modulo-5/exercicios/OOAula04.2/src/one/digitalinnovation/oo/RodarAplicacao.java
@@ -0,0 +1,21 @@
+package one.digitalinnovation.oo;
+
+/**
+ * Classe de exemplo para o exercício da Aula 4 de Orientação a Objetos.
+ */
+
+public class RodarAplicacao {
+
+    public static void main(String[] args) {
+
+        Funcionario funcionario = new Funcionario();
+
+        Funcionario gerente = new Gerente();
+        Funcionario vendedor = new Vendedor();
+        Funcionario faxineiro = new Faxineiro();
+
+        //Gerente gerente_ = new Funcionario();
+        //Vendedor vendedor_ = (Vendedor) new Funcionario();
+    }
+
+}
