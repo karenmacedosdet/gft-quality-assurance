8fd231ef8f8ad6711ace01c4706463c8942c7e3e
kdevmacedo <kdevmacedo@gmail.com>
2023-01-11 01:41:01 -0300

    Atualiza Modulo 5

---
+++ modulo-5/exercicios/OOAula04.5/src/one/digitalinnovation/oo/OperacaoMatematica.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.oo;
+
+/**
+ * Interface de exemplo para o Exercício da Aula 4 de Orientação a Objetos.
+ */
+
+interface OperacaoMatematica {
+
+    void soma(double operando1, double operando2);
+
+    void subtracao(double operando1, double operando2);
+
+    void multiplicacao(double operando1, double operando2);
+
+    void divisao(double operando1, double operando2);
+
+}
