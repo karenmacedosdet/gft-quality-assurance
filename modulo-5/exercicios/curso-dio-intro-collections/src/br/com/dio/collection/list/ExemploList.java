cdc0dab412d6d618a8eac2bb95bdf16409de79c8
kdevmacedo <kdevmacedo@gmail.com>
2023-01-11 17:31:02 -0300

    Atualiza Modulo 5

--- modulo-5/exercicios/curso-dio-intro-collections/src/br/com/dio/collection/list/ExemploList.java
+++ modulo-5/exercicios/curso-dio-intro-collections/src/br/com/dio/collection/list/ExemploList.java
@@ -1,13 +1,17 @@
 package br.com.dio.collection.list;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ExemploList {
 
     public static void main(String[] args) {
 
     //Dada uma lista com 7 notas de um aluno [7, 8.5, 9.3, 5, 7, 0, 3.6], faça:
 
-        //List notas = new ArrayList();
-        //List<Double> notas = new ArrayList<>();
+        //List notas = new ArrayList(); //antes do Java 5
+        //List<Double> notas = new ArrayList<>(); // Denerics(Java 5) - Diamond Operator(Java 7)
         //ArrayList<Double>() notas = new ArrayList<>();
         //List<Double> notas = new ArrayList<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
         /*List<Double> notas = Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
@@ -20,17 +24,34 @@ public class ExemploList {
         System.out.println(notas);
          */
 
-        //System.out.println("Crie uma lista e adicione as sete notas: " + notas);
+        System.out.println("Crie uma lista e adicione as sete notas: ");
 
-        //System.out.println("Exiba a posição da nota 5.0: " + notas.indexOf(5d));
+        List<Double> notas = new ArrayList<Double>();
+        notas.add(7.0);
+        notas.add(8.5);
+        notas.add(9.3);
+        notas.add(5.0);
+        notas.add(7.0);
+        notas.add(0.0);
+        notas.add(3.6);
+        System.out.println(notas.toString());
 
-        //System.out.println("Adicione na lista a nota 8.0 na posição 4: " + notas.add(4, 8d));
+        System.out.println("Exiba a posição da nota 5.0: " + notas.indexOf(5d));
 
-        //System.out.println("Substitua a nota 5.0 pela nota 6.0: " + notas.set(notas.indexOf(5d), 6d));
+        System.out.println("Adicione na lista a nota 8.0 na posição 4: ");
+        notas.add(4, 8d);
+        System.out.println(notas);
 
-        //System.out.println("Confira se a nota 5.0 está na lista: " + notas.contains(5d));
+        System.out.println("Substitua a nota 5.0 pela nota 6.0: ");
+        notas.set(notas.indexOf(5d), 6.0);
+        System.out.println(notas);
 
-        //System.out.println("Exiba todas as notas na ordem em que foram informados: " + notas);
+        System.out.println("Confira se a nota 5.0 está na lista: " + notas.contains(5d));
+
+        /*System.out.println("Exiba todas as notas na ordem em que foram informados: ");
+          for (Double nota : notas) {
+            System.out.println(nota);
+        }*/
 
         //System.out.println("Exiba a terceira nota adicionada: " + notas.get(2));
 
