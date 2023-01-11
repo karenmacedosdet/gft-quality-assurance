e68087c6650c65f2040606e327209c001d247a61
kdevmacedo <kdevmacedo@gmail.com>
2023-01-11 02:14:00 -0300

    Atualiza Modulo 5

---
+++ modulo-5/exercicios/OOAula05.1/src/qualquer/outro/pacote/ExemploPacotes3.java
@@ -0,0 +1,13 @@
+package qualquer.outro.pacote;
+
+import one.digitalinnovation.oo.outropacote.ExemploPacotes1;
+
+/**
+ * Classe de exemplo para o exercício da Aula 5 de Orientação a Objetos.
+ */
+
+public class ExemploPacotes3 {
+
+    ExemploPacotes1 exemploPacotes1;
+
+}
