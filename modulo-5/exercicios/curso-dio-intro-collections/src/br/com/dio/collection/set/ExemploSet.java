6a33cd1314710935c419d33cdf0d8ca18d9d321e
kdevmacedo <kdevmacedo@gmail.com>
2023-01-18 20:33:08 -0300

    Atualiza Modulo 5

--- modulo-5/exercicios/curso-dio-intro-collections/src/br/com/dio/collection/set/ExemploSet.java
+++ modulo-5/exercicios/curso-dio-intro-collections/src/br/com/dio/collection/set/ExemploSet.java
@@ -5,30 +5,30 @@ import java.util.*;
 
 public class ExemploSet {
     public static void main(String[] args) {
-    // Dada uma lista com 7 notas de um aluno [7, 8.5, 9.3, 5, 7, 0, 3.6], faça:
+        // Dada uma lista com 7 notas de um aluno [7, 8.5, 9.3, 5, 7, 0, 3.6], faça:
 
-//      Set notas = new HashSet(); //antes do java 5
-//      HashSet<Double> notas = new HashSet<>();
-//      Set<Double> notas = new HashSet<>(); //Generics(jdk 5) - Diamont Operator(jdk 7)
-/*      Set<Double> notas = Set.of(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
+        //Set notas = new HashSet(); //antes do java 5
+        //HashSet<Double> notas = new HashSet<>();
+        //Set<Double> notas = new HashSet<>(); //Generics(jdk 5) - Diamont Operator(jdk 7)
+        /*Set<Double> notas = Set.of(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
         notas.add(1d);
         notas.remove(5d);
         System.out.println(notas);
-*/
+        */
 
         System.out.println("Crie um conjunto e adicione as notas: ");
         Set<Double> notas = new HashSet<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
         System.out.println(notas.toString());
 
-//        System.out.println("Exiba a posição da nota 5.0: ");
+        //System.out.println("Exiba a posição da nota 5.0: ");
 
-//        System.out.println("Adicione na lista a nota 8.0 na posição 4: ");
+        //System.out.println("Adicione na lista a nota 8.0 na posição 4: ");
 
-//        System.out.println("Substitua a nota 5.0 pela nota 6.0: ");
+        //System.out.println("Substitua a nota 5.0 pela nota 6.0: ");
 
         System.out.println("Confira se a nota 5.0 está no conjunto: " + notas.contains(5d));
 
-//        System.out.println("Exiba a terceira nota adicionada: ");
+        //System.out.println("Exiba a terceira nota adicionada: ");
 
         System.out.println("Exiba a menor nota: " + Collections.min(notas));
 
@@ -48,7 +48,7 @@ public class ExemploSet {
         notas.remove(0d);
         System.out.println(notas);
 
-//        System.out.println("Remova a nota da posição 0");
+        //System.out.println("Remova a nota da posição 0");
 
         System.out.println("Remova as notas menores que 7 e exiba a lista: ");
         Iterator<Double> iterator1 = notas.iterator();
