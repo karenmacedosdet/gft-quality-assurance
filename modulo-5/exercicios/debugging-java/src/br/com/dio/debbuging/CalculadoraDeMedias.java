3837ce78c9712588604963fef7c6a62f1e3b4656
kdevmacedo <kdevmacedo@gmail.com>
2023-01-25 18:20:49 -0300

    Atualiza Modulo 5

---
+++ modulo-5/exercicios/debugging-java/src/br/com/dio/debbuging/CalculadoraDeMedias.java
@@ -0,0 +1,27 @@
+package br.com.dio.debbuging;
+
+import java.util.Scanner;
+
+public class CalculadoraDeMedias {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] alunos = {"Karen", "Laura", "Tiago", "Felipe"};
+
+        double media = calcularMediaDaTurma(alunos, scan);
+
+        System.out.printf("Média da turma: %.1f", media);
+
+    }
+
+    public static double calcularMediaDaTurma(String[] alunos, Scanner scanner) {
+        double soma = 0;
+        for (String aluno : alunos) {
+            System.out.printf("Nota do aluno %s: ", aluno);
+            double nota = scanner.nextDouble();
+            soma += nota;
+        }
+        return soma / alunos.length;
+    }
+
+}
