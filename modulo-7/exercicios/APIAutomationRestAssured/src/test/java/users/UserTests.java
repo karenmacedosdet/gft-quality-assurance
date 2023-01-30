49935ba4131ebeae30289232759108f6affbb8fb
kdevmacedo <kdevmacedo@gmail.com>
2023-01-30 02:41:24 -0300

    Atualiza Modulo 7

--- modulo-7/exercicios/APIAutomationRestAssured/src/test/java/users/UserTests.java
+++ modulo-7/exercicios/APIAutomationRestAssured/src/test/java/users/UserTests.java
@@ -1,115 +1,101 @@
 package users;
 
+import Entities.Booking;
+import Entities.BookingDates;
+import Entities.User;
 import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
 import io.restassured.filter.log.ErrorLoggingFilter;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.*;
-import Entities.User;
-import java.util.*;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
-import static org.junit.jupiter.api.Assertions.*;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.config.LogConfig.logConfig;
-import static org.hamcrest.Matchers.*;
 import static io.restassured.module.jsv.JsonSchemaValidator.*;
+import static org.hamcrest.Matchers.*;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class UserTests {
-
-    private static Faker faker = new Faker();
-
-    private static User user = new User(
-        faker.name().username(),
-        faker.name().firstName(),
-        faker.name().lastName(),
-        faker.internet().safeEmailAddress(),
-        faker.internet().password(8, 10),
-        faker.phoneNumber().toString());
-
-    public static RequestSpecification request;
+class BookingTests {
+    public static Faker faker;
+    private static RequestSpecification request;
+    private static Booking booking;
+    private static BookingDates bookingDates;
+    private static User user;
 
     @BeforeAll
-    public void setup() {
-        RestAssured.baseURI = "petstore.swagger.io/v2";
+    public static void Setup(){
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+        faker = new Faker();
+        user = new User(faker.name().username(),
+            faker.name().firstName(),
+            faker.name().lastName(),
+            faker.internet().safeEmailAddress(),
+            faker.internet().password(8,10),
+            faker.phoneNumber().toString());
+
+        bookingDates = new BookingDates("2018-01-02", "2018-01-03");
+        booking = new Booking(user.getFirstName(), user.getLastName(),
+            (float)faker.number().randomDouble(2, 50, 100000),
+            true,bookingDates,
+            "");
+        RestAssured.filters(new RequestLoggingFilter(),new ResponseLoggingFilter(), new ErrorLoggingFilter());
     }
 
     @BeforeEach
-    public static void setRequest() {
+    void setRequest(){
         request = given().config(RestAssured.config().logConfig(logConfig().enableLoggingOfRequestAndResponseIfValidationFails()))
-            .header("api-key", "special-key")
-            .contentType(ContentType.JSON);
+            .contentType(ContentType.JSON)
+            .auth().basic("admin", "password123");
     }
 
     @Test
-    @Order(1)
-    public void CreateNewUser_WithValidData_ReturnOk() {
-        request.body(user)
+    public void getAllBookingsById_returnOk(){
+        Response response = request
             .when()
-            .post("/user")
+            .get("/booking")
             .then()
-            .assertThat().statusCode(200).and()
-            .body("code", equalTo(200))
-            .body("type", equalTo("unknown"))
-            .body("message", equalTo(user.getUsername()))
-            .body("size()", equalTo(3));
+            .extract()
+            .response();
+
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(200, response.statusCode());
     }
 
     @Test
-    @Order(2)
-    public void GetLogin_ValidUser_ReturnOk() {
+    public void  getAllBookingsByUserFirstName_BookingExists_returnOk(){
         request
-            .param("username", user.getUsername())
-            .param("password", user.getPassword())
             .when()
-            .get("/user/login")
+            .queryParam("firstName", "Carol")
+            .get("/booking")
             .then()
             .assertThat()
             .statusCode(200)
-            .and().time(lessThan(2000L))
-            .and().body(matchesJsonSchemaInClasspath("loginResponseSchema.json"));
+            .contentType(ContentType.JSON)
+            .and()
+            .body("results", hasSize(greaterThan(0)));
+
     }
 
     @Test
-    @Order(3)
-    public void GeUserByName_userIsValid_ReturnOk() {
-        request
-            .when()
-            .get("/user" + user.getUsername())
-            .then()
-            .assertThat().statusCode(200).and().time(lessThan(2000L))
-            .and().body(matchesJsonSchemaInClasspath("loginResponseSchema.json"));
-    }
+    public void  CreateBooking_WithValidData_returnOk(){
 
-    @Test
-    @Order(4)
-    public void DeleteUSer_UserExists_Return200() {
-        request
+        Booking test = booking;
+        given().config(RestAssured.config().logConfig(logConfig().enableLoggingOfRequestAndResponseIfValidationFails()))
+            .contentType(ContentType.JSON)
             .when()
-            .delete("/user" + user.getUsername())
+            .body(booking)
+            .post("/booking")
             .then()
-            .assertThat().statusCode(200).and().time(lessThan(2000L))
-            .log();
-    }
+            .body(matchesJsonSchemaInClasspath("createBookingRequestSchema.json"))
+            .and()
+            .assertThat()
+            .statusCode(200)
+            .contentType(ContentType.JSON).and().time(lessThan(2000L));
 
-    @Test
-    public void CreateNewUser_WithInvalidBody_ReturnBadRequest() {
-        Response response = request
-            .body("teste")
-            .when()
-            .post("/user")
-            .then()
-            .extract().response();
-
-        Assertions.assertNotNull(response);
-        Assertions.assertEquals(400, response.getStatusCode());
-        Assertions.assertEquals(true, response.getBody().asPrettyString().contains("unknown"));
-        Assertions.assertEquals(3, response.getBody().jsonPath().getMap("$").size());
     }
 
 }
